7d1c8ec40ac947a74240f8a6c7bfb44d43144aa4
jeffrepo <jeffer.leo93@gmail.com>
2022-11-22 18:18:38 -0600

    update

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "STECHNOLOGIES",
+
+    'summary': """ STECHNOLOGIES """,
+
+    'description': """
+        https://github.com/jeffrepo/stech.git
+    """,
+
+    'author': "STECHNOLOGIES",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['sale'],
+
+    'data': [
+        # 'views/product_template_views.xml',
+        # 'views/product_views.xml',
+    ],
+    'assets':{
+        'point_of_sale.assets': [
+            # 'pos_technotrade/static/src/js/**/*.js',
+        ],
+        'web.assets_qweb':[
+        ],
+        'web.assets_backend': [
+        ],
+    },
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
